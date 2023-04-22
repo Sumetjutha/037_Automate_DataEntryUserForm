8237e33beaa2cbfdcf64364f82741ca58f217ed7
Sumet Jutajan <sumet.jutha@gmail.com>
2023-04-22 21:18:15 +0700

    Create main.py

---
+++ main.py
@@ -0,0 +1,10 @@
+# import library
+from tkinter import *
+from tkinter.ttk import Combobox
+import tkinter as tk
+from tkinter import messagebox
+import openpyxl, xlrd
+from openpyxl import Workbook
+import pathlib
+
+print("Hello")
