52a266ed0c79f06a23b7012e7f32e6517bca326d
Sumet Jutajan <sumet.jutha@gmail.com>
2023-04-22 21:28:06 +0700

    update

--- main.py
+++ main.py
@@ -7,4 +7,17 @@ import openpyxl, xlrd
 from openpyxl import Workbook
 import pathlib
 
-print("Hello")
+root=Tk()
+root.title("Data Entry")
+root.geometry('700x400+300+200')
+root.resizable(False,False)
+root.configure(bg="#326273")
+
+#icon
+icon_image=PhotoImage(file="free.png")
+root.iconphoto(False,icon_image)
+
+#heading
+Label(root,text="Please fill out this entry form: ", font="arial 13", bg="#326273", fg="#fff").place(x=20, y=20)
+
+root.mainloop()
